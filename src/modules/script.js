7dfca8dad5a8e659d107f0abf4fe5911adcc206f
Oleksii Savchenko <savchenko.orient@gmail.com>
2021-02-09 15:36:25 +0200

    add topMenu to script.js

--- src/modules/script.js
+++ src/modules/script.js
@@ -1,27 +1,47 @@
 'use strict';
 const modalWindow = () => {
-    
-    const modalCallback = document.querySelector('#callback'),
-        modalClose = document.querySelector('.modal-close'),
-        modalOverlay = document.querySelector('.modal-overlay'),
-        callbackBtn = document.querySelectorAll('.callback-btn')[1];
 
-    const showModal = () => {
-        modalCallback.style.display = 'block';
-        modalOverlay.style.display = 'block';
-    };
+	const modalCallback = document.querySelector('#callback'),
+		modalClose = document.querySelector('.modal-close'),
+		modalOverlay = document.querySelector('.modal-overlay'),
+		callbackBtn = document.querySelectorAll('.callback-btn')[1];
 
-    const closeModal = () => {
-        modalCallback.style.display = 'none';
-        modalOverlay.style.display = 'none';
-    };
+	const showModal = () => {
+		modalCallback.style.display = 'block';
+		modalOverlay.style.display = 'block';
+	};
 
-    callbackBtn.addEventListener('click', showModal);
-    modalClose.addEventListener('click', closeModal);
-    modalOverlay.addEventListener('click', closeModal);
+	const closeModal = () => {
+		modalCallback.style.display = 'none';
+		modalOverlay.style.display = 'none';
+	};
 
-    
-    
+	callbackBtn.addEventListener('click', showModal);
+	modalClose.addEventListener('click', closeModal);
+	modalOverlay.addEventListener('click', closeModal);
+
+
+
 };
 
 modalWindow();
+
+const topMenu = () => {
+	const menu = document.querySelector('.top-menu'),
+		menuItems = menu.querySelectorAll('ul>li>a');
+
+	for (const anchor of menuItems) {
+		anchor.addEventListener('click', event => {
+			event.preventDefault();
+
+			const blockID = anchor.getAttribute('href');
+            
+			document.querySelector(blockID).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+		});
+	}
+};
+
+topMenu();
