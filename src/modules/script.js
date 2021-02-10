7f3f4d071d6358f2ceb10d1ec4185f7cf25913be
Oleksii Savchenko <savchenko.orient@gmail.com>
2021-02-10 10:15:23 +0200

    add slider serviseCarousel

--- src/modules/script.js
+++ src/modules/script.js
@@ -23,7 +23,6 @@ const modalWindow = () => {
 
 
 };
-
 modalWindow();
 
 const topMenu = () => {
@@ -43,5 +42,70 @@ const topMenu = () => {
 		});
 	}
 };
-
 topMenu();
+
+const slider = () => {
+    const sliderItems = document.querySelectorAll('.item');
+    
+    let slideIndex = 0,
+        interval,
+        slide = sliderItems[slideIndex];
+    
+    const prevSlide = () => {
+        slide.style.display = 'none';  
+    };
+
+    const nextSlide = () => {
+        slide.style.display = 'block'; 
+    };
+
+    const autoPlay = () => {
+        prevSlide();
+        slideIndex++;
+        if (slideIndex >= sliderItems.length) {
+            slideIndex = 0;
+        }
+        slide = sliderItems[slideIndex];
+        nextSlide();
+    };
+    
+    const runSlider = (time = 3000) => {
+        interval = setInterval(autoPlay, time);
+    };
+    runSlider();
+};
+slider();
+
+const serviseCarousel = () => {
+    const container = document.querySelectorAll('.container')[5],
+        elements = document.querySelectorAll('.col-sm-6.col-md-4');
+    const modalCallback = document.querySelector('#callback'),
+		modalClose = document.querySelector('.modal-close'),
+		modalOverlay = document.querySelector('.modal-overlay'),
+		callbackBtn = document.querySelectorAll('.callback-btn')[1];
+    
+    const showModal = () => {
+		modalCallback.style.display = 'block';
+		modalOverlay.style.display = 'block';
+	};
+
+	const closeModal = () => {
+		modalCallback.style.display = 'none';
+		modalOverlay.style.display = 'none';
+	};
+    
+    container.addEventListener('click', (event) => {
+        let target = event.target;
+        console.log(target);
+        if (target.classList.contains('fancyboxModal')) {
+            showModal();
+	modalClose.addEventListener('click', closeModal);
+	modalOverlay.addEventListener('click', closeModal);
+        }
+        
+    });
+    
+};
+serviseCarousel();
+
+
